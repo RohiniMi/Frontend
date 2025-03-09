4cdc76937ddd1e96e62cf9f3423f8626e158e100
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-03-10 00:08:44 +0530

    nested conditions

---
+++ JS/7_conditions/nestedCondition.js
@@ -0,0 +1,8 @@
+let str = "Hello, rohini ";
+if(str.length != null){
+    if((str.toLowerCase()).includes('h')){
+        console.log("h is present in the string");
+        
+    }
+     
+}
---
+++ JS/7_conditions/positiveOrNegative.js
@@ -0,0 +1,12 @@
+let a = 9;
+if(a==0){
+    console.log("Number is zero");
+    
+}
+if(a>0){
+    console.log("Number is positive"); 
+}
+else{
+    console.log("Number is negative");
+    
+}
