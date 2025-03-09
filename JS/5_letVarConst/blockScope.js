a0c5b4fa616f903b5082b80df88f45f1933cd515
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-03-09 23:02:03 +0530

    block scope behaviour

---
+++ JS/5_letVarConst/blockScope.js
@@ -0,0 +1,13 @@
+{
+    var age = 6;
+    let name = "Rohini";
+    const message = `Hi! there`;
+    console.log("Inside the functionblock");
+    console.log(age, " inside");
+    console.log(name, " inside");
+    console.log(message, " inside");
+}
+console.log("Outside the function");
+console.log(age, " Outside"); //accessible
+// console.log(name, " Outside"); // error
+// console.log(message, " Outside"); //error
