2cb0d09ec9e0559a9813a09850eb75b8a9cdceae
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-03-09 23:05:34 +0530

    reassignment

---
+++ JS/5_letVarConst/reassignment.js
@@ -0,0 +1,11 @@
+var age = 6;
+age = 8;
+console.log(age);
+
+let name = "Rohini";
+name = "Mittal"
+console.log(name);
+
+const message = `Hi! there`;
+// message = "changed"; //error
+console.log(message);
