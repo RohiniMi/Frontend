f8a1e6897a164077d496d62c455d7751a951a3f7
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-03-09 21:45:06 +0530

    dataTypes type checking

---
+++ JS/3_dataTypes/typeChecking.js
@@ -0,0 +1,34 @@
+// - Use `typeof` to check the data types of the following and print each result:
+//     - A string
+//     - A number
+//     - A boolean
+//     - An object
+//     - A symbol
+//     - An undefined variable
+
+const firstName = "Rohini";
+console.log(typeof(firstName));
+
+const num = 67.89;
+console.log(typeof(num));
+
+const boolValue = true;
+console.log(typeof(boolValue));
+
+const data = {
+    name : "Rohini",
+    age : 23,
+    email : "rohinimittal246@gmail.com"
+};
+console.log(typeof(data));
+
+const symbolVar1 = Symbol("Rohini");
+console.log(symbolVar1);
+
+var pi;
+console.log(typeof(pi));
+
+
+
+
+
