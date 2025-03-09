128418c11c4ef5155ed3c2460ef435a8893caa1b
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-03-09 23:08:10 +0530

    redecalration

---
+++ JS/5_letVarConst/redeclaration.js
@@ -0,0 +1,13 @@
+var age = 9;
+var age = 23;
+console.log(age); //successful
+
+let name = "Rohini"; 
+// let name = "Mittal"; // gives error
+console.log(name);
+
+const a = 90;
+// const a = 78; // gives error
+console.log(a);
+
+
