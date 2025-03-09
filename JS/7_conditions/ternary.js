456ebbe70ee4af7bff6124880d19ea02ee4440bc
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-03-10 00:17:12 +0530

    ternary opertor for condition

---
+++ JS/7_conditions/ternary.js
@@ -0,0 +1,15 @@
+let message;
+message = (7 > 8) ? true : false;
+console.log(message);
+
+
+//truthy and Falsy Value
+
+if (NaN) {
+    console.log("Truthy");
+
+}
+else {
+    console.log("Falsy");
+
+}
