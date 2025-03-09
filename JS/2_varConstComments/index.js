07bd352c6db815063fb43559462f3562b1871bd3
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-03-09 15:24:51 +0530

    variables constants and comments

---
+++ JS/2_varConstComments/index.js
@@ -0,0 +1,7 @@
+let message = "Hello JavaScript"; //Declare a variable using `let` to store a string.
+const pi = 3.14; //Declare a variable using `const` to store a number.
+var ans = true; //Declare another variable using `var` to store a boolean value.
+console.log(ans);
+
+/*message = "chenged message";
+pi = 8;*/
