1257c78d620c956af0f1858d888ec2b8463e2cbc
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-03-09 21:37:28 +0530

    dataTypes string

---
+++ JS/3_dataTypes/string.js
@@ -0,0 +1,9 @@
+//1. **String Creation**:
+const firstName = 'Rohini'; //Create a string variable using single quotes.
+const lastName = "Mittal";  //Create another string variable using double quotes.
+const message = `Hii! ${firstName} ${lastName}`;
+
+//2. **String Concatenation**:
+
+const finalString = firstName + " " + lastName + " " + message;
+console.log(finalString);
