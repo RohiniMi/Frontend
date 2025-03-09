d02657e564d6022f5c12b7681e91adcfb69a8f5d
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-03-09 23:43:52 +0530

    arithmetic operators with pre and post increment and decrement

---
+++ JS/6_operators/arithmetic.js
@@ -0,0 +1,44 @@
+let a = 80;
+let b = 10;
+//basic Arithmetic operations
+console.log(`Addition of ${a} and ${b} is ${a+b}`);
+console.log(`Subtraction of ${a} and ${b} is ${a-b}`);
+console.log(`Multiplication of ${a} and ${b} is ${a*b}`);
+console.log(`Division of ${a} and ${b} is ${a/b}`);
+console.log(`Remainder of ${a} by ${b} is ${a%b}`);
+
+//unary negation 
+let negativeValue = -90;
+console.log(`Negation of ${a} is ${0-a}`);
+console.log(`Negation of ${b} is ${0-b}`);
+console.log(`Negation of negative value to positive value of ${negativeValue} is ${0- negativeValue}`);
+
+//Increment and Decrement
+
+let x,y;
+console.log("before any increment or decrement");
+console.log("Value of a is :",a)
+console.log("Value of b is :",b);
+console.log("Value of x is :",x);
+console.log("Value of y is :",y);
+
+x = a++;
+y = ++b;
+console.log("After post increment in a and pre increment in b");
+console.log("Value of a is :",a)
+console.log("Value of b is :",b);
+console.log("Value of x is :",x);
+console.log("Value of y is :",y);
+
+x = a--;
+y = --b;
+console.log("After post decrement in a and pre decrement in b");
+console.log("Value of a is :",a)
+console.log("Value of b is :",b);
+console.log("Value of x is :",x);
+console.log("Value of y is :",y);
+
+
+// console.log();
+
+
