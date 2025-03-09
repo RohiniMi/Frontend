a5f177c0f2f543a061338e6129e6c312fbf1a5e6
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-03-09 22:47:08 +0530

    global scoping

---
+++ JS/5_letVarConst/globalScope.js
@@ -0,0 +1,16 @@
+var age = 6;
+let name = "Rohini";
+const message = `Hi! there`;
+
+console.log(age);
+console.log(name);
+console.log(message);
+
+function print(age, name, message) {
+    console.log("Inside the function");
+    console.log(age);
+    console.log(name);
+    console.log(message);
+}
+
+print(age,name,message);
