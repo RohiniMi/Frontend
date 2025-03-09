746e8cbb6a6c3254a596cbd075ff033f861f9e84
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-03-09 21:40:18 +0530

    dataTypes boolean

---
+++ JS/3_dataTypes/boolean.js
@@ -0,0 +1,8 @@
+//### Part 2: Boolean Logic
+// - Initialize a boolean variable with a `true` value and another with a `false` value.
+// - Print both values to the console.
+
+const a = true;
+const b = false;
+console.log(a,b);
+
